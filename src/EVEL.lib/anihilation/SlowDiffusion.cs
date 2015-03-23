e8659e034f198cef5929b7c749a5e980c2cb65c0
Dawid Giebel <giebel.dawid@op.pl>
2015-03-23 19:56:54 +0100

    [models] Updated slow diffusion model. Added slow diffusion xml configuration.

--- src/EVEL.lib/anihilation/SlowDiffusion.cs
+++ src/EVEL.lib/anihilation/SlowDiffusion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Evel.interfaces;
 using Evel.share;
+using System.Linq;
 
 namespace Evel.engine.anh.stdmodels
 {
@@ -37,13 +38,13 @@ namespace Evel.engine.anh.stdmodels
             ff(x(-t1), c1);
         }
 
-        private void AppendLtParams(double weight, double kapc, double lamPs, double lam_diff,
+        private void AppendLtParams(double kapc, double lamIntr, double lamPs, double lam_diff,
             double sig, double taumin, Func<LTCurveParams> getCurrentParams, LTCurveParams diffsParameter)
         {
 
             if (sig > 0.0)
             {
-                double norm = 1 / Math.Sqrt(2 * Math.PI) / sig;
+                double norm = 0.39894228 / sig; //1 / Math.Sqrt(2 * Math.PI) / sig;
                 double mian = 0.5 / sig / sig;
                 double uu = Math.Log(1 / lamPs + taumin);
 
@@ -51,8 +52,8 @@ namespace Evel.engine.anh.stdmodels
                 {
                     if (Math.Abs(u) < 80)
                     {
-                        double lambda = 1 / (Math.Exp(u) + taumin);
-                        double c = weight * kapc / (lam_diff - lambda) * norm * Math.Exp(-(u - uu) * (u - uu) * mian);
+                        double lambda = 1 / (Math.Exp(u) + taumin) + lamIntr;
+                        double c = _c * kapc / (lam_diff - lamPs) * norm * Math.Exp(-(u - uu) * (u - uu) * mian);
                         LTCurveParams ltp = getCurrentParams();
                         ltp.tau = 1 / lambda;
                         ltp.fraction = c;
@@ -73,11 +74,12 @@ namespace Evel.engine.anh.stdmodels
             }
             else
             {
-                double tmpc = weight * kapc / (lam_diff - lamPs);
+                double lamP = lamPs + lamIntr;
+                double tmpc = kapc / (lam_diff - lamP);
                 LTCurveParams ltp = getCurrentParams();
-                ltp.tau = 1 / lam_diff;
+                ltp.tau = 1 / lamP;
                 ltp.fraction = tmpc;
-                diffsParameter.fraction -= tmpc * lamPs / lam_diff;
+                diffsParameter.fraction -= tmpc * lamP / lam_diff;
             }
         }
 
@@ -165,7 +167,7 @@ namespace Evel.engine.anh.stdmodels
                     new ParameterDefinition("int", ParameterStatus.Local | ParameterStatus.Free, ParameterProperties.Hidden | ParameterProperties.ComponentIntensity),
                     new ParameterDefinition("tauPo1", "[p text='t' font='symbol'][p text='po1' index='sub']", ParameterStatus.Common | ParameterStatus.Free, 0),
                     new ParameterDefinition("sigmaPo1", "[p text='s' font='symbol'][p text='po1' index='sub']", ParameterStatus.Common | ParameterStatus.Free, 0),
-                    new ParameterDefinition("kappa1", "[p text='k' font='symbol'][p text='2' index='sub']", ParameterStatus.Local | ParameterStatus.Free, 0),
+                    new ParameterDefinition("kappa1", "[p text='k' font='symbol'][p text='1' index='sub']", ParameterStatus.Local | ParameterStatus.Free, 0),
                     new ParameterDefinition("tauPo2", "[p text='t' font='symbol'][p text='po2' index='sub']", ParameterStatus.Common | ParameterStatus.Free, 0),
                     new ParameterDefinition("sigmaPo2", "[p text='s' font='symbol'][p text='po2' index='sub']", ParameterStatus.Common | ParameterStatus.Free, 0),
                     new ParameterDefinition("kappa2", "[p text='k' font='symbol'][p text='2' index='sub']", ParameterStatus.Local | ParameterStatus.Free, 0),
@@ -235,12 +237,13 @@ namespace Evel.engine.anh.stdmodels
             double sigma1 = parameters[1].Components[0]["sigmaPo1"].Value;
             double sigma2 = parameters[1].Components[0]["sigmaPo2"].Value;
             double sigmaTrapp = parameters[1].Components[0]["sigmatrapp"].Value;
-            double lambdaortho1 = 1 / parameters[1].Components[0]["tauPo1"].Value + 1.0 / 142.0;
-            double lampara1 = 1 / parameters[1].Components[0]["tauPo1"].Value + 1.0 / 0.124;
-            double lambdaortho2 = 1 / parameters[1].Components[0]["tauPo2"].Value + 1.0 / 142.0;
-            double lampara2 = 1 / parameters[1].Components[0]["tauPo2"].Value + 1.0 / 0.124;
+            double lambdaortho1 = 1 / parameters[1].Components[0]["tauPo1"].Value;
+            double lampara1 = 1 / parameters[1].Components[0]["tauPo1"].Value;
+            double lambdaortho2 = 1 / parameters[1].Components[0]["tauPo2"].Value;
+            double lampara2 = 1 / parameters[1].Components[0]["tauPo2"].Value;
             double lambdaTrapp = 1 / parameters[1].Components[0]["tautrapp"].Value;
-            double lam = 1 / parameters[1].Components[0]["taufree"].Value + kappa1 + kappa2 + mi;
+            double lambdaFree = 1 / parameters[1].Components[0]["taufree"].Value;
+            double lam = lambdaFree + kappa1 + kappa2 + mi;
 
             int currentParamsIndex = 0;
 
@@ -271,53 +274,53 @@ namespace Evel.engine.anh.stdmodels
             if (kappa1 > eps)
             {
                 //ortho-Ps
-                AppendLtParams(0.75, kappa1, lambdaortho1, lam, sigma1, 0.54, GetCurrentParameters, diffsParameters);
+                AppendLtParams(0.75 * kappa1, 1.0 / 142.0, lambdaortho1, lam, sigma1, 0.54, GetCurrentParameters, diffsParameters);
 
                 //para-Ps
-                AppendLtParams(0.25, kappa1, lampara1, lam, 0.0, 0.54, GetCurrentParameters, diffsParameters);
+                AppendLtParams(0.25 * kappa1, 1.0 / 0.125, lampara1, lam, 0.0, 0.0, GetCurrentParameters, diffsParameters);
             }
 
             if (kappa2 > eps)
             {
                 //ortho-Ps
-                AppendLtParams(0.75, kappa2, lambdaortho2, lam, sigma1, 0.54, GetCurrentParameters, diffsParameters);
+                AppendLtParams(0.75 * kappa2, 1.0 / 142.0, lambdaortho2, lam, sigma2, 0.54, GetCurrentParameters, diffsParameters);
 
                 //para-Ps
-                AppendLtParams(0.25, kappa2, lampara2, lam, 0.0, 0.54, GetCurrentParameters, diffsParameters);
+                AppendLtParams(0.25 * kappa2, 1.0 / 0.125, lampara2, lam, 0.0, 0.0, GetCurrentParameters, diffsParameters);
             }
 
             if (mi > eps)
             {
-                AppendLtParams(1, mi, lambdaTrapp, lam, sigmaTrapp, 0.0, GetCurrentParameters, diffsParameters);
+                AppendLtParams(mi, 0, lambdaTrapp, lam, sigmaTrapp, 0.0, GetCurrentParameters, diffsParameters);
             }
 
             //bulk
             {
                 LTCurveParams pfree = GetCurrentParameters();
-                pfree.tau = lam;
-                pfree.fraction = 1 / Math.Abs(parameters[1].Components[0]["taufree"].Value) / lam;
+                pfree.tau = 1 / lambdaFree;
+                pfree.fraction = lambdaFree / lam;
             }
 
-            diffsParameters.tau = lam;
+            diffsParameters.tau = 1 / lam;
 
             double sum = 0.0;
 
             for (int i = 0; i < currentParamsIndex; i++)
             {
                 LTCurveParams p = curveParameters[i] as LTCurveParams;
-                sum += p.fraction;
+                //sum += p.fraction;
                 p.component = parameters[1].Components[0];
             }
 
-            for (int i = 0; i < currentParamsIndex; i++)
-                (curveParameters[i] as LTCurveParams).fraction /= sum;
+            //for (int i = 0; i < currentParamsIndex; i++)
+            //    (curveParameters[i] as LTCurveParams).fraction /= sum;
 
 #if DEBUG
             sum = 0.0;
             for (int i = 0; i < currentParamsIndex; i++)
                 sum += (curveParameters[i] as LTCurveParams).fraction;
 
-            System.Diagnostics.Debug.Assert(Math.Abs(sum - 1.0) < 1e-5);
+            System.Diagnostics.Debug.Assert(Math.Abs(sum - 1.0) < 1e-3);
 #endif
 
             //source components
@@ -372,5 +375,27 @@ namespace Evel.engine.anh.stdmodels
 
             }
         }
+
+#if DEBUG
+        private string SerializeParameters(List<ICurveParameters> curveParameters)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            int i = 0;
+
+            foreach (LTCurveParams p in curveParameters.OfType<LTCurveParams>())
+            {
+                builder.AppendFormat("{0}Parameters {1}{0}\n", new String('-', 10), i);
+                foreach (System.Reflection.FieldInfo f in p.GetType().GetFields().Where(_f => !_f.FieldType.IsClass))
+                {
+                    builder.AppendFormat("{0}={1}\n", f.Name, f.GetValue(p));
+                }
+                builder.AppendLine();
+                i++;
+            }
+
+            return builder.ToString();
+        }
+#endif
     }
 }
