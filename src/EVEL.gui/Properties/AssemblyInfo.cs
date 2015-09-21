2907edda4f3586b27c2c4a9f433968995acf98d7
Dawid Giebel <giebel.dawid@op.pl>
2015-09-21 20:24:03 +0200

    [version] increased versions

--- src/EVEL.gui/Properties/AssemblyInfo.cs
+++ src/EVEL.gui/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.2.2.1")]
-[assembly: AssemblyFileVersion("10.2.2.1")]
+[assembly: AssemblyVersion("10.2.2.3")]
+[assembly: AssemblyFileVersion("10.2.2.3")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
